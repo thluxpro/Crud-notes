d5c5dc09c431edc5bb012ba25d09546b752ea215
thluxpro <lucas_ezes099@hotmail.com>
2023-12-10 22:28:50 -0300

    actualizacion

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,15 +1,33 @@
 //const { use } = require("passport");
 
 const userController = {};
-
 const passport = require ('passport');
 const Usuario = require('../models/Usuario');
 
 userController.renderSingUpForm = (req, res) => {
     res.render('users/signup');
 };
-userController.signup = (req, res) => {
-    res.send('sign up');
+userController.signup = async (req, res) => {
+    const {name, password, email} = req.body;
+
+    try {
+        const usuarioExistente = await Usuario.findOne({email});
+        if (usuarioExistente){
+            req.flash('error_msg', 'El correo electronico ya esta usado');
+            return res.redirect('/users/signup');
+        }
+        const newUser = new Usuario({name: name, password:password, email:email});
+        await newUser.save()
+        req.flash('success_msg',' registro existoso');
+        res.redirect('/users/signin');
+    } catch (error) {
+        console.error(error);
+        req.flash('error_msg',' error intente de nuevo');
+        res.redirect('/users/signup');
+    }
+  
+    
+    res.redirect('/users/signup');
 };
 
 userController.renderSingInForm = (req, res) => {
